897aafd67f4deee8f1009059cca8ae5705e7ba1c
vk-adobe <86674341+vk-adobe@users.noreply.github.com>
2025-07-23 03:37:19 -0400

    Initialize fix-button

---
+++ blocks/commerce-orders-list/commerce-orders-list.js
@@ -0,0 +1,50 @@
+import { render as accountRenderer } from '@dropins/storefront-account/render.js';
+import { OrdersList } from '@dropins/storefront-account/containers/OrdersList.js';
+import { tryRenderAemAssetsImage } from '@dropins/tools/lib/aem/assets.js';
+import { readBlockConfig } from '../../scripts/aem.js';
+import {
+  checkIsAuthenticated,
+  CUSTOMER_LOGIN_PATH,
+  CUSTOMER_ORDER_DETAILS_PATH,
+  CUSTOMER_ORDERS_PATH,
+  CUSTOMER_RETURN_DETAILS_PATH,
+  UPS_TRACKING_URL,
+} from '../../scripts/commerce.js';
+
+// Initialize
+import '../../scripts/initializers/account.js';
+import { rootLink } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  const { 'minified-view': minifiedViewConfig = 'false' } = readBlockConfig(block);
+  const productLink = (productData) => (productData?.product ? rootLink(`/products/${productData.product.urlKey}/${productData.product.sku}`) : rootLink('#'));
+
+  if (!checkIsAuthenticated()) {
+    window.location.href = rootLink(CUSTOMER_LOGIN_PATH);
+  } else {
+    await accountRenderer.render(OrdersList, {
+      minifiedView: minifiedViewConfig === 'true',
+      routeTracking: ({ carrier, number }) => {
+        if (carrier === 'ups') {
+          return `${UPS_TRACKING_URL}?tracknum=${number}`;
+        }
+        return '';
+      },
+      routeOrdersList: () => rootLink(CUSTOMER_ORDERS_PATH),
+      routeOrderDetails: (orderNumber) => rootLink(`${CUSTOMER_ORDER_DETAILS_PATH}?orderRef=${orderNumber}`),
+      routeReturnDetails: ({ orderNumber, returnNumber }) => rootLink(`${CUSTOMER_RETURN_DETAILS_PATH}?orderRef=${orderNumber}&returnRef=${returnNumber}`),
+      routeOrderProduct: productLink,
+      slots: {
+        OrderItemImage: (ctx) => {
+          const anchor = document.createElement('a');
+          anchor.href = productLink(ctx.data);
+          tryRenderAemAssetsImage(ctx, {
+            alias: ctx.data.product.sku,
+            imageProps: ctx.defaultImageProps,
+            wrapper: anchor,
+          });
+        },
+      },
+    })(block);
+  }
+}
