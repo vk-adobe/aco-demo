9e2af4a4d21e0952abbe6f96ffbb8c9ed82d12e6
vk-adobe <86674341+vk-adobe@users.noreply.github.com>
2025-07-23 03:37:15 -0400

    Initialize update-cart-onefour

---
+++ cypress/src/support/index.js
@@ -0,0 +1,10 @@
+import './deleteCustomer';
+import './getUserTokenCookie';
+import './waitForResource';
+import './sessionStorage';
+import './getIFrameField';
+import './waitForImages';
+import './interceptConfig';
+
+import registerCypressGrep from '@cypress/grep'
+registerCypressGrep();
