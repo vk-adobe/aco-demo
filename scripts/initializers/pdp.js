23315c8ebc3d3795534fc7a9c00590a03b0eaabc
vk-adobe <86674341+vk-adobe@users.noreply.github.com>
2025-07-23 03:38:05 -0400

    Initialize e2e-prex-eventing-accs

---
+++ scripts/initializers/pdp.js
@@ -0,0 +1,80 @@
+import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
+import { initializers } from '@dropins/tools/initializer.js';
+import { Image, provider as UI } from '@dropins/tools/components.js';
+import {
+  initialize,
+  setEndpoint,
+  setFetchGraphQlHeaders,
+  fetchProductData,
+} from '@dropins/storefront-pdp/api.js';
+import { initializeDropin } from './index.js';
+import {
+  fetchPlaceholders,
+  commerceEndpointWithQueryParams,
+  getOptionsUIDsFromUrl,
+  getSkuFromUrl,
+  loadErrorPage,
+} from '../commerce.js';
+
+export const IMAGES_SIZES = {
+  width: 960,
+  height: 1191,
+};
+
+await initializeDropin(async () => {
+  // Set Fetch Endpoint (Service)
+  setEndpoint(await commerceEndpointWithQueryParams());
+
+  // Set Fetch Headers (Service)
+  setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('cs') }));
+
+  const sku = getSkuFromUrl();
+  const optionsUIDs = getOptionsUIDsFromUrl();
+
+  const [product, labels] = await Promise.all([
+    fetchProductData(sku, { optionsUIDs, skipTransform: true }).then(preloadImageMiddleware),
+    fetchPlaceholders('placeholders/pdp.json'),
+  ]);
+
+  if (!product?.sku) {
+    return loadErrorPage();
+  }
+
+  const langDefinitions = {
+    default: {
+      ...labels,
+    },
+  };
+
+  const models = {
+    ProductDetails: {
+      initialData: { ...product },
+    },
+  };
+
+  // Initialize Dropins
+  return initializers.mountImmediately(initialize, {
+    sku,
+    optionsUIDs,
+    langDefinitions,
+    models,
+    acdl: true,
+    persistURLParams: true,
+  });
+})();
+
+async function preloadImageMiddleware(data) {
+  const image = data?.images?.[0]?.url?.replace(/^https?:/, '');
+
+  if (image) {
+    await UI.render(Image, {
+      src: image,
+      ...IMAGES_SIZES.mobile,
+      params: {
+        ...IMAGES_SIZES,
+      },
+      loading: 'eager',
+    })(document.createElement('div'));
+  }
+  return data;
+}
